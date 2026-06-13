d2d4319688029a6fc56dbcef8b67e1757ad58ee7
elziodzaya <46269172+elziodzaya@users.noreply.github.com>
2026-06-13 13:47:39 -0600

    Delete aplikasi/utils/supabase_client.py

--- aplikasi/utils/supabase_client.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client
-import streamlit as st
-
-def get_supabase():
-    url = st.secrets["supabase"]["url"]
-    key = st.secrets["supabase"]["key"]
-    return create_client(url, key)
